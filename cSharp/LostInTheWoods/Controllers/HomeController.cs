415c0992ea40b5476acf60db5159d4eb417f9bbd
phil-krull <pkrull@codingdojo.com>
2018-09-12 12:15:44 -0500

    adding structure to Lost in the Woods

---
+++ cSharp/LostInTheWoods/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LostInTheWoods.Models;
+using LostInTheWoods.Factory;
+
+namespace LostInTheWoods.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly TrailFactory trailFactory;
+        public HomeController()
+        {
+            trailFactory = new TrailFactory();
+        }
+        public IActionResult Index()
+        {
+            Trail newTrail = new Trail();
+
+            newTrail.Name = "Trail 4";
+            newTrail.Description = "Description for trail 4";
+            newTrail.Length = 1000;
+            newTrail.Elevation = 5000;
+            newTrail.Longitude = 45;
+            newTrail.Latitude = 45;
+
+            trailFactory.Add(newTrail);
+
+            IEnumerable<Trail> allTrails = trailFactory.FindAll();
+            Console.WriteLine("--------------------------------------------------------------------------------------------");
+            Console.WriteLine($"There are {allTrails.Count()} trails in the database and the names are:");
+            foreach(Trail trail in allTrails)
+            {
+                Console.WriteLine($"The name of the trail is: {trail.Name}");
+            }
+
+            ViewBag.AllTrails = allTrails;
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ cSharp/LostInTheWoods/Factory/TrailFactory.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+using Dapper;
+using System.Data;
+using MySql.Data.MySqlClient;
+using LostInTheWoods.Models;
+ 
+namespace LostInTheWoods.Factory
+{
+    public class TrailFactory
+    {
+        private string connectionString;
+        public TrailFactory()
+        {
+            connectionString = "server=localhost;userid=root;password=root;port=3306;database=DapperWoods;SslMode=None";
+        }
+        internal IDbConnection Connection
+        {
+            get {
+                return new MySqlConnection(connectionString);
+            }
+        }
+        //USERFACTORY CLASS DEFINITION
+ 
+        public void Add(Trail trail)
+        {
+            using (IDbConnection dbConnection = Connection) {
+                string query =  "INSERT INTO trails (name, description, length, elevation, longitude, latitude, created_at, updated_at) VALUES (@Name, @Description, @Length, @Elevation, @Longitude, @Latitude, @Created_At, @Updated_At);";
+                dbConnection.Open();
+                dbConnection.Execute(query, trail);
+            }
+        }
+        public IEnumerable<Trail> FindAll()
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                return dbConnection.Query<Trail>("SELECT * FROM trails");
+            }
+        }
+        public Trail FindByID(int id)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                return dbConnection.Query<Trail>("SELECT * FROM trails WHERE id = @Id", new { Id = id }).FirstOrDefault();
+            }
+        }
+
+    }
+}
